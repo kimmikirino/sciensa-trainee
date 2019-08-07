36a966bc7937654cb10237495b3ef23a6347f961
Jaqueline <jaqueline.kirino@soaexpert.com.brl>
2019-08-07 18:06:01 -0300

    header component

---
+++ React/sciensa-filmes/src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../../assets/imgs/logo-sciensa-pb2.png'
+import './header.css'
+
+const Header = () => (
+  <div class="fixed-menu">
+    <div class="logo-content"><img src={Logo} /></div>
+    <div>asdasdas</div>
+  </div>
+)
+
+export default Header
